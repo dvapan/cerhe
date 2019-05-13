79944c75c0a1e17fbd7fe9e901cf697467de4c2b
Danil Apanovich <dvapan@gmail.com>
2019-05-13 14:59:14 +0800

    Use more numpy instead of python things.

--- polynom.py
+++ polynom.py
@@ -35,23 +35,22 @@ class Polynom:
         if deriv is None:
             deriv = sc.zeros_like(x[0])
         self.var_coeffs = get_deriv_poly_var_coeffs(self.count_var, self.degree, x, deriv)
-        print(x,self.var_coeffs)
-        print(self.coeffs)
         return sc.dot(self.var_coeffs, self.coeffs)
 
     def __call__(self, x, deriv=None):
         return self.fx_var(x, deriv)
 
     def fx_var(self, x, deriv = None):
-        val = self.fx(x,deriv)
+        val = self.fx(x,deriv).reshape(-1,1)
         if self.owner is None:
-            return sc.hstack((val,self.var_coeffs))
+            return sc.hstack([val,self.var_coeffs])
         else:
+            count_exprs = len(val)
             size = self.coeff_size*self.owner.count
             shift = self.coeff_size*self.owner.index_of(self)
-            lzeros = sc.zeros(shift)
-            rzeros = sc.zeros(size - self.coeff_size - shift)
-            return sc.fromiter(chain([val], lzeros, self.var_coeffs, rzeros), float)
+            lzeros = sc.zeros((count_exprs, shift))
+            rzeros = sc.zeros((count_exprs, size - self.coeff_size - shift))
+            return sc.hstack([val, lzeros, self.var_coeffs, rzeros])
 
 @lru_cache()
 def pow_indeces(vars_count, degree):
@@ -72,16 +71,20 @@ def nsum(x):
 
 def construct_element(x, pow_i, num_deriv):
     dpow_i = pow_i - num_deriv
+    filtered = dpow_i
     dpow_i = sc.piecewise(dpow_i,
                           [dpow_i>=0, dpow_i < 0],
                           [lambda x: x, 0])
-    coeff = vfact_div(pow_i, dpow_i)
+    coeff = vfact_div(pow_i, filtered)
     return coeff * x**dpow_i
 
 
 @lru_cache()
 def fact_div(a, b):
-    return sc.multiply.reduce(sc.arange(b+1, a+1))
+    if b >= 0:
+        return sc.multiply.reduce(sc.arange(b+1, a+1))
+    else:
+        return 0
 
 vfact_div = sc.vectorize(fact_div)
 
@@ -89,5 +92,5 @@ vfact_div = sc.vectorize(fact_div)
 
 def get_deriv_poly_var_coeffs(count_var, degree, x, deriv):
     pow_i = pow_indeces(count_var, degree)
-    return sc.array([sc.multiply.reduce(construct_element(xi,pow_i, deriv)) for xi in x])
+    return sc.array([sc.multiply.reduce(construct_element(xi,pow_i, deriv).transpose()) for xi in x])
 
--- tests/test_polynom.py
+++ tests/test_polynom.py
@@ -14,30 +14,32 @@ class TestPolynom(unittest.TestCase):
                       [0,1],
                       [1,0],
                       [1,1]])
-        print(self.poly.fx(x))
-        # print(sc.array(test_data.items()))
-        # for x,fx in test_data.items():
-        #     self.assertEqual(fx,self.poly.fx(sc.array([x])))
+        ans = sc.array([1,6,11,21])
+        sct.assert_equal(ans, self.poly.fx(x))
 
     def test_fx_var_coeffs(self):
-        test_data = {}
-        vc_zeros = sc.zeros(6)
-        vc_zeros[0] = 1
-        test_data[0,0] = sc.array([1,1,0,0,0,0,0])
-        test_data[1,1] = sc.array([21,1,1,1,1,1,1])
-        for x,fx in test_data.items():
-            sct.assert_equal(fx, self.poly.fx_var(x))
+        x = sc.array([[0,0],
+                      [1,1]])
+        fx = sc.array([[1,1,0,0,0,0,0],
+                       [21,1,1,1,1,1,1]])
+        sct.assert_equal(fx, self.poly.fx_var(x))
 
     def test_fx_deriv(self):
         test_data = {}
         test_data[0,0] = sc.array([4,0,0,0,1,0,0]), [1,0]
         test_data[1,1] = sc.array([21, 0,0,0,1,1,2]), [1,0]
-        for x,fx in test_data.items():
-            sct.assert_equal(fx[0], self.poly.fx_var(x, fx[1]))
-        test_data[0,0] = sc.array([2,0,1,0,0,0,0]), [0,1]
-        test_data[1,1] = sc.array([13,0,1,2,0,1,0]), [0,1]
-        for x,fx in test_data.items():
-            sct.assert_equal(fx[0], self.poly.fx_var(x, fx[1]))
+        x = sc.array([[0,0],
+                      [1,1]])
+        fx = sc.array([[4,0,0,0,1,0,0],
+                      [21, 0,0,0,1,1,2]])
+        deriv = [1,0]
+        sct.assert_equal(fx, self.poly.fx_var(x, deriv))
+        deriv = [0,1]
+        x = sc.array([[0,0],
+                      [1,1]])
+        fx = sc.array([[2,0,1,0,0,0,0],
+                       [13,0,1,2,0,1,0]])
+        sct.assert_equal(fx, self.poly.fx_var(x, deriv))
 
     def test_scipy_array_equals(self):
         self.assertTrue(sc.any(sc.arange(10) == sc.arange(10)))
