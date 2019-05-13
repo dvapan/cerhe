3d917b92ae4357f1bcb6a1ba70c07d7c8d042241
Danil Apanovich <dvapan@gmail.com>
2019-05-13 14:59:14 +0800

    Use more numpy instead of python things.

--- tests/test_system.py
+++ tests/test_system.py
@@ -15,6 +15,14 @@ class TestContextPolynoms(unittest.TestCase):
         self.context.assign(self.poly1)
         self.context.assign(self.poly2)
 
+        self.x = sc.array([[0,0],
+                      [1,1]])
+        self.fx1 = sc.array([[1, 1,0,0,0,0,0,0,0,0,0,0,0],
+                       [21,1,1,1,1,1,1,0,0,0,0,0,0]])
+        self.fx2 = sc.array([[1, 0,0,0,0,0,0,1,0,0,0,0,0],
+                        [21,0,0,0,0,0,0,1,1,1,1,1,1]])
+
+        
     def test_creation(self):
         p = Polynom(2,2)
         self.assertIsNone(p.owner)
@@ -22,26 +30,11 @@ class TestContextPolynoms(unittest.TestCase):
         self.assertIs(self.poly2.owner,self.context)
 
     def test_slice(self):
-        test_data1 = dict()
-        test_data1[0,0] = sc.array([1, 1,0,0,0,0,0,0,0,0,0,0,0])
-        test_data1[1,1] = sc.array([21,1,1,1,1,1,1,0,0,0,0,0,0])
-        test_data2 = dict()
-        test_data2[0,0] = sc.array([1, 0,0,0,0,0,0,1,0,0,0,0,0])
-        test_data2[1,1] = sc.array([21,0,0,0,0,0,0,1,1,1,1,1,1])
-
-        for x,fx in test_data1.items():
-            sct.assert_equal(fx, self.poly1(x))
-        for x,fx in test_data2.items():
-            sct.assert_equal(fx, self.poly2(x))
+        sct.assert_equal(self.fx1, self.poly1(self.x))
+        sct.assert_equal(self.fx2, self.poly2(self.x))
 
     def test_addition(self):
-        test_data = dict()
-        test_data[0,0] = sc.array([1, 1,0,0,0,0,0,0,0,0,0,0,0])
-        test_data[1,1] = sc.array([21,1,1,1,1,1,1,0,0,0,0,0,0])
-        test_data[0,0] += sc.array([1, 0,0,0,0,0,0,1,0,0,0,0,0])
-        test_data[1,1] += sc.array([21,0,0,0,0,0,0,1,1,1,1,1,1])
-        for x,fx in test_data.items():
-            sct.assert_equal(fx, self.poly1(x)+self.poly2(x))
+        sct.assert_equal(self.fx1 + self.fx2, self.poly1(self.x) + self.poly2(self.x))
 
     def test_expr(self):
         def expr(x,deriv=None):
