fa18b8efe3dec294b7592e792ab9a69cdc599f3c
Danil Apanovich (MESI) <dvapan@gmail.com>
2020-02-11 15:27:44 +0800

    refactoring. Change constants to more phisicaly based. checker save output to file

--- checker3d.py
+++ checker3d.py
@@ -12,10 +12,10 @@ regsize = 0
 
 pc = np.loadtxt("poly_coeff_3d")
 
-tgp = Polynom(2, 5)
-tcp = Polynom(3, 5)
-tgr = Polynom(2, 5)
-tcr = Polynom(3, 5)
+tgp = Polynom(2, 7)
+tcp = Polynom(3, 7)
+tgr = Polynom(2, 7)
+tcr = Polynom(3, 7)
 
 context = Context()
 context.assign(tgp)
@@ -49,30 +49,40 @@ def tcr2tcp(x, p):
 
 
 def main():
+
+    # X = sc.linspace(0, length, 100)
+    # T = sc.linspace(0, time, 100)
+    # R = sc.linspace(radius_inner, radius, 14)
+    # R = R[::-1]
+
+
     var_num = 0
     for el in [tgp,tcp,tgr,tcr]:
         var_num += el.coeff_size
     print(var_num,1)
     # print("residual: ", pc[-1])
     var_num += 1
+
+    f = open("tbl", "w+")
     
     info = " ","t","r","temp","balance left", "residual"
-    fmts = "|{:8}{:7.3}{:7.3}{:12.2f}{:16.6}{:16.6}"
-    info_fmt = "|{:8}{:>7}{:>7}{:>12}{:>16}{:>16}"
-    w = 1+8+7*2+12*1+16*2
+    fmts = "|{:8}{:12.3}{:12.3}{:12.2f}{:16.6}{:16.6}"
+    info_fmt = "|{:8}{:>12}{:>12}{:>12}{:>16}{:>16}"
+    w = 1+8+12*3+16*2
     space = " "*((w+1)*len(X))
     s = "{:<{w}}".format("primal",w=((w+1)*len(X)))
-    print (s)
-    print ("="*len(s))
+    f.write (s+"\n")
+    f.write ("="*len(s)+"\n")
 
+    max_residual = 0
     
     for t in T:
         for x in X:
-            print("{:^{w}.3}".format(x,w=w), end=" ")
-        print()
+            f.write("{:^{w}.3}".format(x,w=w) + " ")
+        f.write("\n")
         for x in X:
-            print(info_fmt.format(*info),end = " ")
-        print()
+            f.write(info_fmt.format(*info) + " ")
+        f.write("\n")
         
         for x in X:
             tg = tgp([x,t])[0]
@@ -83,8 +93,9 @@ def main():
             eq_right = -coef["k2"] * (dtgdx * coef["wg"] + dtgdt)
             row_type = "gas2gasp"
             d = eq_right-eq_left
-            print(fmts.format(row_type,t,"",tg,eq_left,d),end = " ")
-        print()
+            max_residual = max(d/coeffs[row_type],max_residual)
+            f.write(fmts.format(row_type,t,"",tg,eq_left,d) + " ")
+        f.write("\n")
         
         for r in R:
             for x in X:
@@ -101,20 +112,22 @@ def main():
                     eq_right = coef["a"]*(d2tcdr2 + 2/r * dtcdr)
                     eq_left = dtcdt
                     row_type = "cer2cer"
-                print(fmts.format(row_type,"",r,tc,eq_left,eq_right-eq_left),end = " ")
-            print()
-        print(space)
+                d = eq_right-eq_left
+                max_residual = max(d/coeffs[row_type],max_residual)                
+                f.write(fmts.format(row_type,"",r,tc,eq_left,d) + " ")
+            f.write("\n")
+        f.write(space+"\n")
 
-    print ("{:<{w}}".format("reverse",w=(w*len(X))))
-    print ("="*len(s))
+    f.write ("{:<{w}}".format("reverse",w=(w*len(X))) + "\n")
+    f.write ("="*len(s) + "\n")
 
     for t in T:
         for x in X:
-            print("{:^{w}.3}".format(x,w=w), end=" ")
-        print()
+            f.write("{:^{w}.3}".format(x,w=w) + " ")
+        f.write("\n")
         for x in X:
-            print(info_fmt.format(*info),end = " ")
-        print()
+            f.write(info_fmt.format(*info) + " ")
+        f.write("\n")
         
         for x in X:
             tg = tgr([x,t])[0]
@@ -124,10 +137,11 @@ def main():
             eq_left = (tg - tc)*coef["k1"]
             eq_right = coef["k2"] * (dtgdx * coef["wg"] + dtgdt)
             row_type = "gas2gasr"
-            eq_right /= coeffs[row_type]
-            eq_left /= coeffs[row_type]
-            print(fmts.format(row_type,t,"",tg,eq_left,eq_right-eq_left),end = " ")
-        print()
+            d = eq_right-eq_left
+            max_residual = max(d/coeffs[row_type],max_residual)
+            
+            f.write(fmts.format(row_type,t,"",tg,eq_left,d) + " ")
+        f.write("\n")
         
         for r in R:
             for x in X:
@@ -144,14 +158,17 @@ def main():
                     eq_right = coef["a"]*(d2tcdr2 + 2/r * dtcdr)
                     eq_left = dtcdt
                     row_type = "cer2cer"
-                eq_right /= coeffs[row_type]
-                eq_left /= coeffs[row_type]
-                print(fmts.format(row_type,"",r,tc,eq_left,eq_right-eq_left),end = " ")
-            print()
-        print(space)
+                d = eq_right-eq_left
+                max_residual = max(d/coeffs[row_type],max_residual)
+                f.write(fmts.format(row_type,"",r,tc,eq_left,d) + " ")
+            f.write("\n")
+        f.write(space + "\n")
 
-    print ("="*len(s))
+    f.write ("="*len(s) + "\n")
 
+    f.close()
+
+    print("maximum residual: ",max_residual)
         
 if __name__ == "__main__":
     main()
--- solver3d.py
+++ solver3d.py
@@ -6,12 +6,13 @@ from pprint import pprint
 import lp_utils as lut
 from constants import *
 from polynom import Polynom, Context
+import sys
 
 regsize = 0
-tgp = Polynom(2, 5)
-tcp = Polynom(3, 5)
-tgr = Polynom(2, 5)
-tcr = Polynom(3, 5)
+tgp = Polynom(2, 7)
+tcp = Polynom(3, 7)
+tgr = Polynom(2, 7)
+tcr = Polynom(3, 7)
 
 context = Context()
 context.assign(tgp)
@@ -19,13 +20,22 @@ context.assign(tcp)
 context.assign(tgr)
 context.assign(tcr)
 
+pp = [tgp,tcp]
+pr = [tgr,tcr]
+
+var_num = 0
+for el in [tgp,tcp,tgr,tcr]:
+    print(el.coeff_size)
+    var_num+=el.coeff_size
+
+    
+print(var_num,1)
+var_num+=1
+
+
 
 def cer2cer(x,p):
-    return p[1](x,[0, 1, 0]) - coef["a"]*(p[1](x,[0,0,2]) + 2/x[2] * p[1](x,[0,0,1]))
-
-def cer2cerz(x,p):
-    return p[1](x,[0, 0, 1])
-    
+    return p[1](x,[0, 1, 0]) - coef["a"]*(p[1](x,[0,0,2]) + 2/x[2] * p[1](x,[0,0,1])) 
     
 def gas2gasr(x,p):
     return (p[0](x[:-1]) - p[1](x)) * coef["k1"] - coef["k2"] * (p[0](x[:-1], [1, 0]) * coef["wg"] + p[0](x[:-1], [0, 1]))
@@ -39,16 +49,16 @@ def gas2cer(x, p):
 
 def tcp2tcr(x, p):
     x1 = x[0],T[-1],x[1]
-    r1 = tcp(x1)
+    r1 = p[0](x1)
     x2 = x[0],T[0],x[1]
-    r2 = tcr(x2)
+    r2 = p[1](x2)
     return r2 - r1
 
 def tcr2tcp(x, p):
     x1 = x[0],T[-1],x[1]
-    r1 = tcr(x1)
+    r1 = p[0](x1)
     x2 = x[0],T[ 0],x[1]
-    r2 = tcp(x2)
+    r2 = p[1](x2)
     return r2 - r1
 
 
@@ -65,7 +75,6 @@ coeffs = {
     "gas2gasr" : balance_coeff,
     "gas2cer"  : cer_coeff,
     "cer2cer"  : cer_coeff,
-    "cer2cerz" : cer_coeff
 }
 
 def add_residual(var_num, monoms, val=0):
@@ -83,40 +92,29 @@ def add_residuals(var_num, domain, diff_eq, p=None, val=0):
 #        val /= coeffs[diff_eq.__name__]
         add_residual(var_num,r[1:],val)
 
-def main():    
-    pp = [tgp,tcp]
-    pr = [tgr,tcr]
-
-    var_num = 0
-    for el in [tgp,tcp,tgr,tcr]:
-        var_num+=el.coeff_size
-
-    
-    print(var_num,1)
-    var_num+=1
-    
+        
+def main():        
     print ("primal process")
     add_residuals(var_num, product(X,T,R[:1]),gas2gasp,pp)
     add_residuals(var_num, product(X,T,R[:1]),gas2cer,pp)
     add_residuals(var_num, product(X,T,R[1:]),cer2cer,pp)
-    # add_residuals(var_num, product(X,T,R[-1:]),cer2cerz,pp)    
     add_residuals(var_num, product(X[:1],T),tgp,pp,TGZ)
-    add_residuals(var_num, product(X,R),tcp2tcr,pp)
+    add_residuals(var_num, product(X,R),tcp2tcr,[tcp,tcr])
 
     print ("reverse process")
     add_residuals(var_num, product(X,T,R[:1]),gas2gasr,pr)
     add_residuals(var_num, product(X,T,R[:1]),gas2cer,pr)
     add_residuals(var_num, product(X,T,R[1:]),cer2cer,pr)
-    # add_residuals(var_num, product(X,T,R[-1:]),cer2cerz,pr)    
+
     add_residuals(var_num, product(X[-1:],T),tgr,pr,val=TBZ)
-    add_residuals(var_num, product(X,R),tcr2tcp,pr)
+    add_residuals(var_num, product(X,R),tcr2tcp,[tcr,tcp])
         
     prb = sc.vstack(prb_chain)
     x,dx,dz = lut.slvlprd(prb, var_num, TGZ,False)
     pc = sc.split(x[:-1],max_reg)
-
+    residual = x[-1]
+    
     sc.savetxt("poly_coeff_3d",pc)
-
     
 
 if __name__ == "__main__":
